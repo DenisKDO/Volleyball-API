374effd530479e667fa07e835b08999bbafefbda
DenisKDO <den_kim@kbtu.kz>
2024-03-12 13:13:19 +0600

    Adding comments

--- pkg/methods/GetTeams.go
+++ pkg/methods/GetTeams.go
@@ -8,13 +8,18 @@ import (
 )
 
 func GetTeams(w http.ResponseWriter, r *http.Request) {
+	//json
 	w.Header().Set("Content-Type", "application/json")
+
+	//status of request
 	w.WriteHeader(http.StatusOK)
 
+	//adding changes to database
 	var teams []essences.Team
 
 	database.Db.Find(&teams)
 
+	//writing response
 	jsonResponse, err := json.Marshal(teams)
 	if err != nil {
 		return
