354c7140d7448f1f9401333bcb57ab689f87d6eb
DenisKDO <den_kim@kbtu.kz>
2024-03-12 13:13:52 +0600

    Adding routing, methods to API

--- cmd/Volleyball-API/main.go
+++ cmd/Volleyball-API/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"github.com/DenisKDO/Vollyball-API/internal/database"
+	"github.com/DenisKDO/Vollyball-API/pkg/methods"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
 func main() {
+	//connection to database
+	database.Database()
+
+	//api routing
+	router := mux.NewRouter()
+
+	//teams methods
+	router.HandleFunc("/teams", methods.GetTeams).Methods("GET")
+	router.HandleFunc("/team/{id}", methods.GetTeam).Methods("GET")
+	router.HandleFunc("/team/create", methods.CreateTeam).Methods("POST")
+	router.HandleFunc("/team/delete/{id}", methods.DeleteTeam).Methods("DELETE")
+	router.HandleFunc("/team/update/{id}", methods.UpdateTeam).Methods("PUT")
+
+	//players methods
+	router.HandleFunc("/players", methods.GetPlayers).Methods("GET")
+
+	//start api
+	http.ListenAndServe(":8080", router)
+
+	//close connection to database
+	defer database.Db.Close()
+
 }
