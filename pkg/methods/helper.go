e75e9ff63a3e12975163acac030035aadfd9515e
DenisKDO <den_kim@kbtu.kz>
2024-05-02 00:59:35 +0600

    Adding files to filter

---
+++ pkg/methods/helper.go
@@ -0,0 +1,28 @@
+package methods
+
+import (
+	"github.com/DenisKDO/Vollyball-API/pkg/essences"
+	"github.com/jinzhu/gorm"
+	"net/http"
+	"strconv"
+)
+
+func StrToInt(queryStr string, w http.ResponseWriter, parameter string) int {
+	queryInt, err := strconv.Atoi(queryStr)
+
+	if err != nil {
+		http.Error(w, "Invalid "+parameter, http.StatusBadRequest)
+		return 0
+	}
+	return queryInt
+}
+
+func NoRecordsFind(db *gorm.DB, w http.ResponseWriter, parameter string) int {
+	var count int
+	db.Model(&essences.Player{}).Count(&count)
+	if count == 0 {
+		http.Error(w, "No records found matching the "+parameter, http.StatusNotFound)
+		return count
+	}
+	return 1
+}
